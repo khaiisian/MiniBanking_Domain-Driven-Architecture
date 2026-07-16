1d276f572944b7b96cc569f8c39625e3264a61ec
Khai Sian Taang <khaisian.khaisiantaang@gmail.com>
2026-07-17 00:45:46 +0630

    Add and test Money value object with Add and Subtract methods

--- MiniBankingSystem.ConsoleApp/Program.cs
+++ MiniBankingSystem.ConsoleApp/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using MiniBankingSystem.Domain.ValueObjects;
+
+Console.WriteLine("Hello, World!");
+
+var hundredUSD = Money.Of(100, "USD");
+var fifty = Money.Of(50, "USD");
+
+var addResult = hundredUSD.Add(fifty);
+var subtractResult = hundredUSD.Subtract(Money.Of(300, "USD"));
+
+Console.WriteLine("Add Result => " + addResult.Amount + addResult.Currency);
+Console.WriteLine("Subtract Result => " + subtractResult.Amount + subtractResult.Currency);
---
+++ MiniBankingSystem.Domain/ValueObjects/Money.cs
@@ -0,0 +1,54 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace MiniBankingSystem.Domain.ValueObjects;
+
+// Value Object (interchangeable and immutable object)
+// use record since a class does NOT compare by value by default (records give value equality for free)
+
+public record Money
+{
+    public decimal Amount { get; }
+    public string Currency { get; }
+
+    private Money(decimal amount, string currency)
+    {
+        Amount = amount;
+        Currency = currency;
+    }
+
+    public static Money Of(decimal amount, string currency)
+    {
+        if (amount < 0)
+        {
+            throw new ArgumentException("Amount cannot be negative.");
+        }
+
+        if (string.IsNullOrWhiteSpace(currency))
+        {
+            throw new ArgumentException("Currency cannot empty.");
+        }
+
+        return new Money(amount, currency);
+    }
+
+    public Money Add(Money other)
+    {
+        if (other.Currency != Currency)
+        {
+            throw new InvalidOperationException("Cannot add different currencies.");
+        }
+
+        return new Money(Amount + other.Amount, Currency);
+    }
+
+    public Money Subtract(Money other)
+    {
+        if (other.Currency != Currency)
+        {
+            throw new InvalidOperationException("Cannot subtract different currencies.");
+        }
+
+        return Money.Of(Amount - other.Amount, Currency);
+    }
+}
+
