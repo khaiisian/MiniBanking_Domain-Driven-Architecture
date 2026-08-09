8bd973f9a9cd69f87d1fc32944cc342ffbcc75a7
Khai Sian Taang <khaisian.khaisiantaang@gmail.com>
2026-08-09 22:24:47 +0630

    add InMemoryAccountRepository

---
+++ MiniBankingSystem.Infrastructure/Repositories/InMemoryAccountRepository.cs
@@ -0,0 +1,23 @@
+﻿using MiniBankingSystem.Domain.Entities;
+using MiniBankingSystem.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniBankingSystem.Infrastructure.Repositories;
+
+public class InMemoryAccountRepository
+{
+    private readonly Dictionary<AccountId, Account> _accounts = new();
+
+    public void Save (Account account)
+    {
+        _accounts[account.Id] = account;
+    }
+
+    public Account? GetById(AccountId id)
+    {
+        _accounts.TryGetValue(id, out Account account);
+        return account;
+    }
+}
