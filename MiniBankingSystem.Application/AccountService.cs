f9491c3992f5c91b7434f217c97fb70fba48cb6b
Khai Sian Taang <khaisian.khaisiantaang@gmail.com>
2026-08-10 11:12:26 +0630

    add AccountService

---
+++ MiniBankingSystem.Application/AccountService.cs
@@ -0,0 +1,56 @@
+﻿using MiniBankingSystem.Domain;
+using MiniBankingSystem.Domain.Entities;
+using MiniBankingSystem.Domain.Repositories;
+using MiniBankingSystem.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniBankingSystem.Application;
+
+public class AccountService
+{
+    private readonly IAccountRepository _accountRepository;
+
+    public AccountService(IAccountRepository accountRepository)
+    {
+        _accountRepository = accountRepository;
+    }
+
+    public Account OpenAccount(string currency)
+    {
+        var account = Account.Open(currency);
+        _accountRepository.Save(account);
+        return account;
+    }
+
+    public void Deposit(AccountId id, decimal amount, string currency)
+    {
+        var account = _accountRepository.GetById(id);
+
+        if(account is null)
+        {
+            throw new DomainException("Account Not Found.");
+        }
+
+        var money = Money.Of(amount, currency);
+        account.Deposit(money);
+
+        _accountRepository.Save(account);
+    }
+
+    public void Withdraw(AccountId id, decimal amount, string currency)
+    {
+        var account = _accountRepository.GetById(id);
+
+        if (account is null)
+        {
+            throw new DomainException("Account Not Found.");
+        }
+
+        var money = Money.Of(amount, currency);
+        account.Withdraw(money);
+
+        _accountRepository.Save(account);
+    }
+}
--- MiniBankingSystem.ConsoleApp/Program.cs
+++ MiniBankingSystem.ConsoleApp/Program.cs
@@ -1,12 +1,30 @@
-﻿using MiniBankingSystem.Domain.ValueObjects;
+﻿using MiniBankingSystem.Application;
+using MiniBankingSystem.Domain.ValueObjects;
+using MiniBankingSystem.Infrastructure.Repositories;
 
 Console.WriteLine("Hello, World!");
 
-var hundredUSD = Money.Of(100, "USD");
-var fifty = Money.Of(50, "USD");
+//var hundredUSD = Money.Of(100, "USD");
+//var fifty = Money.Of(50, "USD");
 
-var addResult = hundredUSD.Add(fifty);
-var subtractResult = hundredUSD.Subtract(Money.Of(50, "USD"));
+//var addResult = hundredUSD.Add(fifty);
+//var subtractResult = hundredUSD.Subtract(Money.Of(50, "USD"));
 
-Console.WriteLine("Add Result => " + addResult.Amount + addResult.Currency);
-Console.WriteLine("Subtract Result => " + subtractResult.Amount + subtractResult.Currency);
+//Console.WriteLine("Add Result => " + addResult.Amount + addResult.Currency);
+//Console.WriteLine("Subtract Result => " + subtractResult.Amount + subtractResult.Currency);
+
+var repository = new InMemoryAccountRepository();
+var service = new AccountService(repository);
+
+// Create Account
+var account = service.OpenAccount("Kyats");
+var accountId = account.Id;
+Console.WriteLine(accountId.Value);
+
+// Deposit
+service.Deposit(accountId, 500000, "Kyats");
+Console.WriteLine(account.Balance);
+
+// Withdraw
+service.Withdraw(accountId, 100000, "Kyats");
+Console.WriteLine(account.Balance);
